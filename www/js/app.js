6baac618be05f42e9e36617d20b3e4786112b354
Christopher Groskopf <staringmonkey@gmail.com>
2015-07-19 20:47:34 -0400

    Sort of works. Sort of.

--- www/js/app.js
+++ www/js/app.js
@@ -1,47 +1,158 @@
-// Global jQuery references
-var $commentCount = null;
+// Globals
+var resizeSidebarEl = null;
+var resizeMobileEl = null;
+var resizeDesktopEl = null;
+var resizeFluidEl = null;
+var previewWrapperEl = null;
+
+var headlineEl = null;
+var subhedEl = null;
+var footerEl = null;
+var ticksYEl = null;
+var csvEl = null;
+
+var embedEl = null;
+
+var pymParent = null;
+var embedTemplate = null;
 
 /*
- * Run on page load.
+ * Setup editor and preview.
  */
-var onDocumentLoad = function(e) {
-    // Cache jQuery references
-    $commentCount = $('.comment-count');
+var onWindowLoaded = function() {
+    resizeSidebarEl = d3.select('#resize-sidebar');
+    resizeMobileEl = d3.select('#resize-mobile');
+    resizeDesktopEl = d3.select('#resize-desktop');
+    resizeFluidEl = d3.select('#resize-fluid');
+    previewWrapperEl = d3.select('#preview-wrapper');
 
-    renderExampleTemplate();
-    getCommentCount(showCommentCount);
+    headlineEl = d3.select('#headline');
+    subhedEl = d3.select('#subhed');
+    footerEl = d3.select('#footer');
+    ticksYEl = d3.select('#ticks-y');
+    csvEl = d3.select('#csv');
 
-    SHARE.setup();
-}
+    embedEl = d3.select('#embed');
 
-/*
- * Basic templating example.
- */
-var renderExampleTemplate = function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4),
-        'copy': JSON.stringify(COPY, null, 4)
+    embedTemplate = _.template(d3.select('#embed-template').html());
+
+    resizeSidebarEl.on('click', function() {
+        previewWrapperEl.style.width = '180px';
+        pymParent.sendWidth();
+
+        window.location.hash = 'sidebar';
     });
 
-    var html = JST.example(context);
+    resizeMobileEl.on('click', function() {
+        previewWrapperEl.style.width = '300px';
+        pymParent.sendWidth();
 
-    $('#template-example').html(html);
+        window.location.hash = 'mobile';
+    });
+
+    resizeDesktopEl.on('click', function() {
+        previewWrapperEl.style.width = '730px';
+        pymParent.sendWidth();
+
+        window.location.hash = 'desktop';
+    });
+
+    resizeFluidEl.on('click', function() {
+        previewWrapperEl.style.width = '100%';
+        pymParent.sendWidth();
+
+        window.location.hash = 'fluid';
+    });
+
+    if (window.location.hash == '#sidebar') {
+        previewWrapperEl.style.width = '180px';
+    } else if (window.location.hash == '#mobile') {
+        previewWrapperEl.style.width = '300px';
+    } else if (window.location.hash == '#desktop') {
+        previewWrapperEl.style.width = '730px';
+    } else if (window.location.hash == '#fluid') {
+        previewWrapperEl.style.width = '100%';
+    }
+
+    pymParent = new pym.Parent('preview', 'graphic.html', {});
+    pymParent.onMessage('ready', updateChart);
+
+    var changers = [headlineEl, subhedEl, footerEl, ticksYEl, csvEl];
+
+    for (var i = 0; i < changers.length; i++) {
+        changers[i].on('change', updateChart);
+    }
 }
 
 /*
- * Display the comment count.
+ * Validate user input for chart parameters.
  */
-var showCommentCount = function(count) {
-    $commentCount.text(count);
-
-    if (count > 0) {
-        $commentCount.addClass('has-comments');
+var validate = function() {
+    if (isNaN(parseInt(ticksYEl[0][0].value))) {
+        return 'TicksY is not a number!';
     }
 
-    if (count > 1) {
-        $commentCount.next('.comment-label').text('Comments');
+    if (!csvEl[0][0].value) {
+        return 'No CSV data!';
     }
 }
 
-$(onDocumentLoad);
+/*
+ * Validate the chart inputs, rerender it and update the embed code.
+ */
+var updateChart = function() {
+    var error = validate();
+
+    if (error) {
+        alert(error);
+        return;
+    }
+
+    var configuration = {
+        'COPY': {
+            'labels': {
+                'headline': headlineEl[0][0].value,
+                'subhed': subhedEl[0][0].value,
+                'footer': footerEl[0][0].value,
+                'source': null,
+                'credit': null
+            }
+        },
+        'GRAPHIC_DATA': csvEl[0][0].value,
+        'PARAMETERS': {
+            'ticksY': parseInt(ticksYEl[0][0].value)
+        }
+    }
+
+    pymParent.sendMessage('configure', JSON.stringify(configuration));
+
+    generateEmbedCode(configuration);
+}
+
+/*
+ * Generate new embed code for the current chart configuration.
+ */
+var generateEmbedCode = function(configuration) {
+    var scriptTag = function(src) {
+        var tag = '<script type="text/javascript"';
+
+        if (src) {
+            tag += ' src="' + src + '"';
+        }
+
+        return tag + '>';
+    }
+
+    var closeScript = function() {
+        return '</script>';
+    }
+
+    embedEl.text(embedTemplate({
+        'scriptTag': scriptTag,
+        'closeScript': closeScript,
+        'slug': 'test-graphic',
+        'configuration': configuration
+    }))
+}
+
+window.onload = onWindowLoaded;
